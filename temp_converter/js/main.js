e447d5e8f7fe98d1fe990b21b506b173bba49794
mikeqle <vulcun@VULCUNs-MacBook-Air-6.local>
2016-04-07 21:22:20 -0700

    04.07.2015.001 Lab

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,9 +1,45 @@
 // Fahrenheit to Celsius
+var input,output;
+
 $('#fahrenheit_to_celsius').on('click', function () {
-  var fah = $('#temperature').val();
-  fah = parseFloat(fah);
-// Convert to celsius
-  var cels;
-  cels = (fah - 32)*5/9;
-  $('#result').html(cels);
+  input = $('#temperature').val();
+  input = parseFloat(input);
+
+  if (isNaN(input)) {
+    $('#result').html('Input is not a number!');
+    return;
+  }
+// Convert math
+  output = (input - 32)*5/9;
+  $('#result').html(convertText(input)+'F='+convertText(output)+'C');
+  weather(input);
 });
+
+$('#celsius_to_fahrenheit').on('click', function () {
+  input = $('#temperature').val();
+  input = parseFloat(input);
+  if (isNaN(input)) {
+    $('#result').html('Input is not a number!');
+    return;
+  }
+// Convert math
+  output = (input*1.8 +32);
+  $('#result').html(convertText(input)+'C='+convertText(output)+'F');
+  weather(output);
+});
+
+function convertText (number) {
+  var converted = number.toFixed(0) + '&deg;';
+  return converted;
+}
+
+function weather (number) {
+  if (number>70) {
+    $('body').addClass('hot');
+    $('body').removeClass('cold');
+  }
+  else {
+    $('body').addClass('cold');
+    $('body').removeClass('hot');
+  }
+}
