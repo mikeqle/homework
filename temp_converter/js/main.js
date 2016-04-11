d5e662fd4d3b4f49426cf3a877b42d75665b7729
mikeqle <vulcun@VULCUNs-MacBook-Air-6.local>
2016-04-11 13:33:26 -0700

    04.11.2016.001 FINAL

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,10 +1,10 @@
-// Fahrenheit to Celsius
+// Declare input / output
 var input,output;
 
 $('#fahrenheit_to_celsius').on('click', function () {
-  input = $('#temperature').val();
+  input = $('#temperature').val(); // retrieve input
   input = parseFloat(input);
-
+// Check if not number
   if (isNaN(input)) {
     $('#result').html('Input is not a number!');
     return;
@@ -12,12 +12,13 @@ $('#fahrenheit_to_celsius').on('click', function () {
 // Convert math
   output = (input - 32)*5/9;
   $('#result').html(convertText(input)+'F='+convertText(output)+'C');
-  weather(input);
+  weather(input); // change color according to Fahrenheit
 });
 
 $('#celsius_to_fahrenheit').on('click', function () {
-  input = $('#temperature').val();
+  input = $('#temperature').val(); // retrieve input
   input = parseFloat(input);
+// Check if not number
   if (isNaN(input)) {
     $('#result').html('Input is not a number!');
     return;
@@ -25,14 +26,16 @@ $('#celsius_to_fahrenheit').on('click', function () {
 // Convert math
   output = (input*1.8 +32);
   $('#result').html(convertText(input)+'C='+convertText(output)+'F');
-  weather(output);
+  weather(output); // change color according to Fahrenheit
 });
 
+// Function to conver numbers to degree format
 function convertText (number) {
   var converted = number.toFixed(0) + '&deg;';
   return converted;
 }
 
+// Determines whether or not weather is cold/hot (over/under 70 degrees) and change background
 function weather (number) {
   if (number>70) {
     $('body').addClass('hot');
